f65f4a7ec597c4714ac8cb2b04ba765e6c632bba
heloisakuntz <167546781+heloisakuntz@users.noreply.github.com>
2024-10-25 08:27:26 -0300

    25.10

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao (
+    'Carros',
+    'Qual o carro esportivo mais vendido?',
+    'Porshe911'
+)
+criaCartao (
+    'Pais',
+    'Qual o pais mais visitado por turistas?',
+    'Franca'
+)
+criaCartao (
+    'Caminhao',
+    'Qual o caminhao mais vendido em 2024?',
+    'Volvo fh 540'
+)
+criaCartao (
+    'Estado',
+    'Qual o maior estado do Brasil hoje em dia?',
+    'Amazonas'
+)
