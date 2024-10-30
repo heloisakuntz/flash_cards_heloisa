3ee0304409a84a86b6efba276c3c3233ee38ae0e
heloisakuntz <167546781+heloisakuntz@users.noreply.github.com>
2024-10-30 09:26:05 -0300

    30-10-2024

--- aap.js
+++ aap.js
@@ -1,18 +1,18 @@
 function criaCartao(categoria, pergunta, resposta){
-    let container = document.getElementById('container'); 
-    let catao = document.createElement('article'); 
-    catao.className = 'cartao';
-
-    catao.innerHTML = `
-     <div class="cartao__conteudo">
-                    <h3>${categoria}</h3>
-                    <div class="cartao__conteudo__pergunta">
-                        <p>${pergnta}</p>
-                    </div>
-                    <div class="cartao__conteudo__resposta">
-                        <p>${resposta}</p>
-                    </div>
-                </div>
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+  
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+                      <h3>${categoria}</h3>
+                      <div class="cartao__conteudo__pergunta">
+                          <p>${pergunta}</p>
+                      </div>
+                      <div class="cartao__conteudo__resposta">
+                          <p>${resposta}</p>
+                      </div>
+                  </div>
     `
     container.appendChild(cartao);
-}
+  }
